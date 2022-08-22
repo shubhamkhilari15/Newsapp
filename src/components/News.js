96df11a7f8aebcc94651c4bb10cef2331adc1f52
Shubham Khilari <shubhamkhilari14@gmail.com>
2022-08-22 22:38:31 +0530

    added spinner comp and spinner image

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
  
@@ -16,33 +17,41 @@ export class News extends Component {
   
   }
  async componentDidMount(){
+  this.setState({loading:true});
     let data =await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=30a1926b7373433faa37a25b326991d4&page=${this.state.page}&pageSize=${this.props.pageSize}`);
     let parsedData=await data.json();
     this.setState({articles:parsedData.articles,
-      totalResults:parsedData.totalResults
+      totalResults:parsedData.totalResults,
+      loading:false
     });
   console.log(parsedData);
   }
   onPrevClick = async ()=>{
+    this.setState({loading:true});
     let data =await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=30a1926b7373433faa37a25b326991d4&page=${this.state.page-1}&pageSize=${this.props.pageSize}`);
     let parsedData=await data.json();
     this.setState({articles:parsedData.articles,
-    page:this.state.page-1});
+    page:this.state.page-1,
+    loading:false
+});
     console.log(this.state.page);
   }
   onNextClick = async ()=>{
+    this.setState({loading:true});
     let data =await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=30a1926b7373433faa37a25b326991d4&page=${this.state.page+1}&pageSize=${this.props.pageSize}`);
     let parsedData=await data.json();
     this.setState({articles:parsedData.articles,
-    page:this.state.page+1});
+    page:this.state.page+1,
+    loading: false});
     console.log(this.state.page);
   }
   render() {
     return (
       <div className="container my-3">
         <h1 className="text-center" style={{margin:'20px'}}>NewsDose- TOP HeadLines</h1>
+        {this.state.loading&&<Spinner/>}
         <div className="row">
-         {this.state.articles.map((element)=>{ return <div className="col-md-4 my-3" key={element.url}>
+         {!this.state.loading &&this.state.articles.map((element)=>{ return <div className="col-md-4 my-3" key={element.url}>
           {/* map() fun expect arrow func as parameter which reutun element with key ....
           and here we are returning one div everytime...
           so we need key to indetify the each div */}
---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Loading from './loading.gif'
+export class Spinner extends Component {
+  render() {
+    return (
+      <div className='text-center'>
+        <img src={Loading} alt="loading"/>
+      </div>
+    )
+  }
+}
+
+export default Spinner
